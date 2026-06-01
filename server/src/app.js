8d010a6c2db2a5712f9f10548f9a0d143af27c97
TranDinhDat <trandat0601dh@gmail.com>
2026-06-01 09:02:07 +0700

    feat: enable LAN host, relative API paths, and dynamic CORS for network testing

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ import { userRoutes } from "./routes/user.routes.js";
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(logger);
